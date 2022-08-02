413baba15f8b3cd886f9eb0d278431b9d4b66230
harriheikkinen <harri.heikkinen@talentree.fi>
2022-08-02 19:19:43 +0300

    add content, lint

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,70 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <Transition appear name="slide-right">
+      <AboutCard />
+    </Transition>
+    <Transition appear name="slide-right">
+      <CertificationsCard />
+    </Transition>
+    <Transition appear name="slide-left">
+      <TechCard />
+    </Transition>
+  </v-container>
 </template>
 
+<script>
+import { defineComponent } from "vue";
+import AboutCard from "../components/AboutCard.vue";
+import TechCard from "../components/TechCard.vue";
+import CertificationsCard from "../components/CertificationsCard.vue";
+
+export default defineComponent({
+  name: "AboutView",
+  components: {
+    AboutCard,
+    TechCard,
+    CertificationsCard,
+  },
+});
+</script>
+
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.main-card {
+  margin: auto;
+  margin-top: 12px;
+  padding: 12px;
+  max-width: 700px;
+}
+
+p {
+  text-align: left;
+}
+
+.slide-right-enter-active {
+  transition: all 0.3s ease-in;
+}
+
+.slide-right-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-right-enter-from,
+.slide-right-leave-to {
+  transform: translateX(-20px);
+  opacity: 0;
+}
+
+.slide-left-enter-active {
+  transition: all 0.3s ease-in;
+}
+
+.slide-left-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-left-enter-from,
+.slide-left-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
 }
 </style>
